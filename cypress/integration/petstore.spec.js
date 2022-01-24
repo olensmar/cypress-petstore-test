688acdf03f56b549cab37a714efb94c20513c3f6
olensmar <ole@lensmar.com>
2022-01-24 17:00:36 +0100

    Initial commit

---
+++ cypress/integration/petstore.spec.js
@@ -0,0 +1,9 @@
+describe('Petstore Testing', () => {
+    it('Visits PetStore', () => {
+        cy.visit('http://localhost:8001')
+        cy.get('.title').should('have.text', "Failed to load API definition.")
+        cy.get('.download-url-input').type("{selectall}http://localhost:8001/api/swagger.json")
+        cy.get('.download-url-button').click()
+        cy.get('.title').should('contain.text', "Swagger Petstore")
+    })
+})
